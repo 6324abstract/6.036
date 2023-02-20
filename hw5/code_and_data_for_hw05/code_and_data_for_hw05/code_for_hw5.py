987c50d8086b752f328a5f6e4c61e1c79eb909d2
Yanzhe Li <41003148+6324abstract@users.noreply.github.com>
2023-02-20 15:52:27 +0900

    hw5 finished

--- hw5/code_and_data_for_hw05/code_and_data_for_hw05/code_for_hw5.py
+++ hw5/code_and_data_for_hw05/code_and_data_for_hw05/code_for_hw5.py
@@ -1,7 +1,7 @@
-
 import numpy as np
 import csv
 import itertools, functools, operator
+
 
 # Takes a list of numbers and returns a column vector:  n x 1
 def cv(value_list):
@@ -15,6 +15,7 @@ def cv(value_list):
     """
     return np.transpose(rv(value_list))
 
+
 # Takes a list of numbers and returns a row vector: 1 x n
 def rv(value_list):
     """ Return a 1 x d np array.
@@ -24,6 +25,7 @@ def rv(value_list):
     array([[1, 2, 3]])
     """
     return np.array([value_list])
+
 
 # In all the following definitions:
 # x is d by n : input data
@@ -43,6 +45,7 @@ def lin_reg(x, th, th0):
     """
     return np.dot(th.T, x) + th0
 
+
 def square_loss(x, y, th, th0):
     """ Returns the squared loss between y_pred and y
 
@@ -52,7 +55,8 @@ def square_loss(x, y, th, th0):
     >>> square_loss(X, Y, th, th0).tolist()
     [[4.2025, 3.4224999999999985, 5.0625, 3.8025000000000007]]
     """
-    return (y - lin_reg(x, th, th0))**2
+    return (y - lin_reg(x, th, th0)) ** 2
+
 
 def mean_square_loss(x, y, th, th0):
     """ Return the mean squared loss between y_pred and y
@@ -64,7 +68,8 @@ def mean_square_loss(x, y, th, th0):
     [[4.1225]]
     """
     # the axis=1 and keepdims=True are important when x is a full matrix
-    return np.mean(square_loss(x, y, th, th0), axis = 1, keepdims = True)
+    return np.mean(square_loss(x, y, th, th0), axis=1, keepdims=True)
+
 
 def ridge_obj(x, y, th, th0, lam):
     """ Return the ridge objective value
@@ -79,7 +84,8 @@ def ridge_obj(x, y, th, th0, lam):
     >>> ridge_obj(X, Y, th, th0, 100.).tolist()
     [[104.37250000000002]]
     """
-    return np.mean(square_loss(x, y, th, th0), axis = 1, keepdims = True) + lam * np.linalg.norm(th)**2
+    return np.mean(square_loss(x, y, th, th0), axis=1, keepdims=True) + lam * np.linalg.norm(th) ** 2
+
 
 def d_lin_reg_th(x, th, th0):
     """ Returns the gradient of lin_reg(x, th, th0) with respect to th
@@ -96,8 +102,9 @@ def d_lin_reg_th(x, th, th0):
     >>> d_lin_reg_th(X, th, th0).tolist()
     [[1.0, 2.0, 3.0, 4.0], [1.0, 1.0, 1.0, 1.0]]
     """
-    #Your code here
+    # Your code here
     return x
+
 
 def d_square_loss_th(x, y, th, th0):
     """Returns the gradient of square_loss(x, y, th, th0) with respect to
@@ -121,8 +128,9 @@ def d_square_loss_th(x, y, th, th0):
     [[4.1, 7.399999999999999, 13.5, 15.600000000000001], [4.1, 3.6999999999999993, 4.5, 3.9000000000000004]]
 
     """
-    #Your code here
-    return 2*(th.T*x+th0-y)*x
+    # Your code here
+    return 2 * x * (lin_reg(x, th, th0) - y)
+
 
 def d_mean_square_loss_th(x, y, th, th0):
     """ Returns the gradient of mean_square_loss(x, y, th, th0) with
@@ -142,8 +150,9 @@ def d_mean_square_loss_th(x, y, th, th0):
     # print("X =", repr(X))
     # print("Y =", repr(Y))
     # print("th =", repr(th), "th0 =", repr(th0))
-    #Your code here
-    return np.mean(d_square_loss_th(x,y,th,th0),axis=-1,keepdims=True)
+    # Your code here
+    return np.mean(d_square_loss_th(x, y, th, th0), axis=-1, keepdims=True)
+
 
 def d_lin_reg_th0(x, th, th0):
     """ Returns the gradient of lin_reg(x, th, th0) with respect to th0.
@@ -153,8 +162,9 @@ def d_lin_reg_th0(x, th, th0):
     >>> d_lin_reg_th0(x, th, th0).tolist()
     [[1.0, 1.0, 1.0, 1.0]]
     """
-    #Your code here
-    return rv(np.ones(x[:,0].size))
+    # Your code here
+    return rv(np.ones(x[0, :].size))
+
 
 def d_square_loss_th0(x, y, th, th0):
     """ Returns the gradient of square_loss(x, y, th, th0) with
@@ -168,8 +178,9 @@ def d_square_loss_th0(x, y, th, th0):
     >>> d_square_loss_th0(X, Y, th, th0).tolist()
     [[4.1, 3.6999999999999993, 4.5, 3.9000000000000004]]
     """
-    #Your code here
-    return 2*(th.T*x+th0-y)
+    # Your code here
+    return d_lin_reg_th0(x, th, th0) * 2 * (lin_reg(x, th, th0) - y)
+
 
 def d_mean_square_loss_th0(x, y, th, th0):
     """ Returns the gradient of mean_square_loss(x, y, th, th0) with
@@ -181,8 +192,9 @@ def d_mean_square_loss_th0(x, y, th, th0):
     >>> d_mean_square_loss_th0(X, Y, th, th0).tolist()
     [[4.05]]
     """
-    #Your code here
-    return np.mean(d_square_loss_th0(x,y,th,th0),axis=-1,keepdims=True)
+    # Your code here
+    return np.mean(d_square_loss_th0(x, y, th, th0), axis=-1, keepdims=True)
+
 
 def d_ridge_obj_th(x, y, th, th0, lam):
     """Return the derivative of tghe ridge objective value with respect
@@ -200,8 +212,9 @@ def d_ridge_obj_th(x, y, th, th0, lam):
     >>> d_ridge_obj_th(X, Y, th, th0, 100.).tolist()
     [[210.15], [14.05]]
     """
-    #Your code here
-    pass
+    # Your code here
+    return d_mean_square_loss_th(x, y, th, th0) + 2 * lam * th
+
 
 def d_ridge_obj_th0(x, y, th, th0, lam):
     """Return the derivative of tghe ridge objective value with respect
@@ -219,14 +232,16 @@ def d_ridge_obj_th0(x, y, th, th0, lam):
     >>> d_ridge_obj_th0(X, Y, th, th0, 100.).tolist()
     [[4.05]]
     """
-    #Your code here
-    pass
+    # Your code here
+    return d_mean_square_loss_th0(x, y, th, th0)
 
-#Concatenates the gradients with respect to theta and theta_0
+
+# Concatenates the gradients with respect to theta and theta_0
 def ridge_obj_grad(x, y, th, th0, lam):
     grad_th = d_ridge_obj_th(x, y, th, th0, lam)
     grad_th0 = d_ridge_obj_th0(x, y, th, th0, lam)
-    return np.vstack([grad_th, grad_th0])    
+    return np.vstack([grad_th, grad_th0])
+
 
 def sgd(X, y, J, dJ, w0, step_size_fn, max_iter):
     """Implements stochastic gradient descent
@@ -258,46 +273,65 @@ def sgd(X, y, J, dJ, w0, step_size_fn, max_iter):
     ws: the list of values of www found during all the iterations
 
     """
-    #Your code here
-    pass
+
+    # Your code here
+    def sgd(X, y, J, dJ, w0, step_size_fn, max_iter):
+        ws, fs = [], []
+        w = w0
+        for i in range(0, max_iter):
+            n = np.random.randint(X.shape[1])
+            ws.append(w)
+            fs.append(J(X[:, n:n + 1], y[:, n:n + 1], w))
+            w = w - step_size_fn(i) * dJ(X[:, n:n + 1], y[:, n:n + 1], w)
+
+        return w, fs, ws
+
 
 ############################################################
-#From HW04; Used in the test case for sgd, below
+# From HW04; Used in the test case for sgd, below
 def num_grad(f):
     def df(x):
         g = np.zeros(x.shape)
         delta = 0.001
         for i in range(x.shape[0]):
-            xi = x[i,0]
-            x[i,0] = xi - delta
+            xi = x[i, 0]
+            x[i, 0] = xi - delta
             xm = f(x)
-            x[i,0] = xi + delta
+            x[i, 0] = xi + delta
             xp = f(x)
-            x[i,0] = xi
-            g[i,0] = (xp - xm)/(2*delta)
+            x[i, 0] = xi
+            g[i, 0] = (xp - xm) / (2 * delta)
         return g
+
     return df
 
-#Test case for sgd
+
+# Test case for sgd
 def sgdTest():
     def downwards_line():
         X = np.array([[0.0, 0.1, 0.2, 0.3, 0.42, 0.52, 0.72, 0.78, 0.84, 1.0],
-                      [1.0, 1.0, 1.0, 1.0, 1.0,  1.0,  1.0,  1.0,  1.0,  1.0]])
+                      [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0]])
         y = np.array([[0.4, 0.6, 1.2, 0.1, 0.22, -0.6, -1.5, -0.5, -0.5, 0.0]])
         return X, y
-    
+
     X, y = downwards_line()
-    
+
+    def svm_min_step_size_fn(i):
+        return 0.01 / (i + 1) ** 0.5
+
     def J(Xi, yi, w):
         # translate from (1-augmented X, y, theta) to (separated X, y, th, th0) format
-        return float(ridge_obj(Xi[:-1,:], yi, w[:-1,:], w[-1:,:], 0))
-    
+        return float(ridge_obj(Xi[:-1, :], yi, w[:-1, :], w[-1:, :], 0))
+
     def dJ(Xi, yi, w):
         def f(w): return J(Xi, yi, w)
+
         return num_grad(f)(w)
 
-    #Insert code to call sgd on the above
-    pass
+    # Insert code to call sgd on the above
+    return sgd(X, y, J, dJ, w0=np.ones((X.shape[0],1)),step_size_fn=svm_min_step_size_fn, max_iter=1000)
+
+
 
 ############################################################
 
@@ -307,22 +341,24 @@ def ridge_min(X, y, lam):
     Assumes that X is NOT 1-extended. Interfaces to our sgd by 1-extending
     and building corresponding initial weights.
     """
+
     def svm_min_step_size_fn(i):
-        return 0.01/(i+1)**0.5
+        return 0.01 / (i + 1) ** 0.5
 
     d, n = X.shape
     X_extend = np.vstack([X, np.ones((1, n))])
-    w_init = np.zeros((d+1, 1))
+    w_init = np.zeros((d + 1, 1))
 
     def J(Xj, yj, th):
-        return float(ridge_obj(Xj[:-1,:], yj, th[:-1,:], th[-1:,:], lam))
+        return float(ridge_obj(Xj[:-1, :], yj, th[:-1, :], th[-1:, :], lam))
 
     def dJ(Xj, yj, th):
-        return ridge_obj_grad(Xj[:-1,:], yj, th[:-1,:], th[-1:,:], lam)
-    
+        return ridge_obj_grad(Xj[:-1, :], yj, th[:-1, :], th[-1:, :], lam)
+
     np.random.seed(0)
     w, fs, ws = sgd(X_extend, y, J, dJ, w_init, svm_min_step_size_fn, 1000)
-    return w[:-1,:], w[-1:,:]
+    return w[:-1, :], w[-1:, :]
+
 
 #######################################################################
 
@@ -333,36 +369,41 @@ def mul(seq):
     '''
     return functools.reduce(operator.mul, seq, 1)
 
+
 def make_polynomial_feature_fun(order):
     '''
     Transform raw features into polynomial features or order 'order'.
     If raw_features is a d by n numpy array, return a k by n numpy array 
     where k = sum_{i = 0}^order multichoose(d, i) (the number of all possible terms in the polynomial feature or order 'order')
     '''
+
     def f(raw_features):
         d, n = raw_features.shape
-        result = []   # list of column vectors
+        result = []  # list of column vectors
         for j in range(n):
             features = []
-            for o in range(1, order+1):
+            for o in range(1, order + 1):
                 indexTuples = \
-                          itertools.combinations_with_replacement(range(d), o)
+                    itertools.combinations_with_replacement(range(d), o)
                 for it in indexTuples:
                     features.append(mul(raw_features[i, j] for i in it))
             result.append(cv(features))
         return np.hstack(result)
+
     return f
+
 
 ######################################################################
 
-#First finds a predictor on X_train and X_test using the specified value of lam
-#Then runs on X_test, Y_test to find the RMSE
+# First finds a predictor on X_train and X_test using the specified value of lam
+# Then runs on X_test, Y_test to find the RMSE
 def eval_predictor(X_train, Y_train, X_test, Y_test, lam):
     th, th0 = ridge_min(X_train, Y_train, lam)
     return np.sqrt(mean_square_loss(X_test, Y_test, th, th0))
 
-#Returns the mean RMSE from cross validation given a dataset (X, y), a value of lam,
-#and number of folds, k
+
+# Returns the mean RMSE from cross validation given a dataset (X, y), a value of lam,
+# and number of folds, k
 def xval_learning_alg(X, y, lam, k):
     '''
     Given a learning algorithm and data set, evaluate the learned classifier's score with k-fold
@@ -377,19 +418,20 @@ def xval_learning_alg(X, y, lam, k):
     idx = list(range(n))
     np.random.seed(0)
     np.random.shuffle(idx)
-    X, y = X[:,idx], y[:,idx]
+    X, y = X[:, idx], y[:, idx]
 
     split_X = np.array_split(X, k, axis=1)
     split_y = np.array_split(y, k, axis=1)
 
     score_sum = 0
     for i in range(k):
-        X_train = np.concatenate(split_X[:i] + split_X[i+1:], axis=1)
-        y_train = np.concatenate(split_y[:i] + split_y[i+1:], axis=1)
+        X_train = np.concatenate(split_X[:i] + split_X[i + 1:], axis=1)
+        y_train = np.concatenate(split_y[:i] + split_y[i + 1:], axis=1)
         X_test = np.array(split_X[i])
         y_test = np.array(split_y[i])
         score_sum += eval_predictor(X_train, y_train, X_test, y_test, lam)
-    return score_sum/k
+    return score_sum / k
+
 
 ######################################################################
 # For auto dataset
@@ -409,6 +451,7 @@ def load_auto_data(path_data):
             data.append(datum)
     return data
 
+
 # Feature transformations
 def std_vals(data, f):
     '''
@@ -423,10 +466,11 @@ def std_vals(data, f):
     (6.5, 2.5)
     '''
     vals = [entry[f] for entry in data]
-    avg = sum(vals)/len(vals)
-    dev = [(entry[f] - avg)**2 for entry in data]
-    sd = (sum(dev)/len(vals))**0.5
+    avg = sum(vals) / len(vals)
+    dev = [(entry[f] - avg) ** 2 for entry in data]
+    sd = (sum(dev) / len(vals)) ** 0.5
     return (avg, sd)
+
 
 def standard(v, std):
     '''
@@ -438,7 +482,8 @@ def standard(v, std):
     >>> standard(data, (3,1))
     [array([-2., -1.,  2.,  4.,  5.])]
     '''
-    return [(v-std[0])/std[1]]
+    return [(v - std[0]) / std[1]]
+
 
 def raw(x):
     '''
@@ -448,6 +493,7 @@ def raw(x):
     [[1, 2, 3, 4]]
     '''
     return [x]
+
 
 def one_hot(v, entries):
     '''
@@ -460,16 +506,17 @@ def one_hot(v, entries):
     >>> one_hot(1, range(5))
     [0, 1, 0, 0, 0]
     '''
-    vec = len(entries)*[0]
+    vec = len(entries) * [0]
     vec[entries.index(v)] = 1
     return vec
+
 
 # The class (mpg) added to the front of features
 def auto_data_and_values(auto_data, features):
     features = [('mpg', raw)] + features
-    std = {f:std_vals(auto_data, f) for (f, phi) in features if phi==standard}
-    entries = {f:list(set([entry[f] for entry in auto_data])) \
-               for (f, phi) in features if phi==one_hot}
+    std = {f: std_vals(auto_data, f) for (f, phi) in features if phi == standard}
+    entries = {f: list(set([entry[f] for entry in auto_data])) \
+               for (f, phi) in features if phi == one_hot}
     vals = []
     for entry in auto_data:
         phis = []
@@ -484,15 +531,20 @@ def auto_data_and_values(auto_data, features):
     data_labels = np.vstack(vals)
     return data_labels[:, 1:].T, data_labels[:, 0:1].T
 
+
 ######################################################################
 
-#standardizes a row vector of y values
-#returns both the standardized vector and the mean, variance
+# standardizes a row vector of y values
+# returns both the standardized vector and the mean, variance
 def std_y(row):
     '''
     >>> std_y(np.array([[1,2,3,4]]))
     (array([[-1.34164079, -0.4472136 ,  0.4472136 ,  1.34164079]]), array([2.5]), array([1.11803399]))
     '''
     mu = np.mean(row, axis=1)
-    sigma = np.sqrt(np.mean((row - mu)**2, axis=1))
-    return np.array([(val - mu)/(1.0*sigma) for val in row]), mu, sigma
+    sigma = np.sqrt(np.mean((row - mu) ** 2, axis=1))
+    return np.array([(val - mu) / (1.0 * sigma) for val in row]), mu, sigma
+
+
+if __name__=="__main__":
+  print(sgdTest())
